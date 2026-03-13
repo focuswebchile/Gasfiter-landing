c06f4e38f3d9aa8abbb1c2b57f3636a29378308a
Felipe Ibar <focuswebchile@gmail.com>
2026-03-13 11:10:46 +0100

    Remove remaining ABCIS runtime coupling from Gasfiter

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-const CMS_HOST = "cms.abcis.cl";
-const CMS_STAGING_PATH = "/staging?slug=abcis";
-
-export function middleware(request: NextRequest) {
-  const hostHeader = request.headers.get("host") || "";
-  const hostname = hostHeader.split(":")[0].toLowerCase();
-  const { pathname } = request.nextUrl;
-
-  if (hostname === CMS_HOST && pathname === "/") {
-    const url = request.nextUrl.clone();
-    url.pathname = "/staging";
-    url.search = "slug=abcis";
-    return NextResponse.redirect(url, 308);
-  }
-
+export function middleware(_request: NextRequest) {
   return NextResponse.next();
 }
 
